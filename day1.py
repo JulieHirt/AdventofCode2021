1318f1567a686267fc1954e3a68a23e42492a1d6
epicpython <jr3python@gmail.com>
2022-01-01 23:52:57 -0500

    finished day 1 part 2

--- day1.py
+++ day1.py
@@ -1,3 +1,6 @@
+#day 1 part 2
+
+import math 
 #read from text file
 text_file = open("day1data.txt", "r")
 lines = text_file.readlines()
@@ -9,55 +12,34 @@ for line in lines:
     newline = int(line)
     betterlines.append(newline)
 
-print(betterlines)
-#divide into sliding sets of 3 and print all sets
-#for i in range(len(betterlines)):
-for i in range(5):
-    index1 = i
-    index2 = i+1
-    index3 = i+2
-    print("indexes", index1," ",index2, " ", index3)
-    print("set of 3 numbers",betterlines[index1]," ", betterlines[index2]," ", betterlines[index3])
-
-
-#old code
-"""
-number1A = betterlines[0]
-number2A = betterlines[1]
-number3A = betterlines[2]
-
-number1B = betterlines[3]
-number2B = betterlines[4]
-number3B = betterlines[5]
-print("len")
-print((len(betterlines)))
-
-def averageof3(num1,num2,num3):
-    total = num1+ num2 +num3
-    average =total/3
-    return average
+#print(betterlines)
 
 def sumof3(num1,num2,num3):
     total = num1+ num2 +num3
     return total
 
-print(str(averageof3(3,2,3)))
+prevsum = math.inf #set first sum to infinity so it will not measure an increase
+nextsum = 0
+numincreases =0 #use to count the number of times the sliding sum increased
 
-numincreases = 0
-for i in range(0, len(betterlines)-2):
-    number1B = betterlines[i]
-    number2B = betterlines[i+1]
-    number3B = betterlines[i+2]
-    if sumof3(number1A,number2A,number3A) < sumof3(number1B,number2B,number3B):
+#divide into sliding sets of 3 and print all sets
+#for i in range(len(betterlines)):
+for i in range(len(betterlines)-2):
+    index1 = i
+    index2 = i+1
+    index3 = i+2
+    nextsum = sumof3(betterlines[index1], betterlines[index2], betterlines[index3])
+    if nextsum > prevsum:
         numincreases += 1
-    number1A = number2A
-    number2A = number3A
-    number3A = number1B
-           
-print("numincreases is",numincreases)
+    prevsum= nextsum #set up for next time in loop
+    
+    #print("indexes", index1," ",index2, " ", index3)
+    #print("set of 3 numbers",betterlines[index1]," ", betterlines[index2]," ", betterlines[index3])
+    
+    
 
 
-#higher than 180
-#lower than 1807
-"""
+print("numincreases is", numincreases)
+
+
 input("press enter to exit")
