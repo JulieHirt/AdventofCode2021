40457e6946e4af36e1bdd7fb21c85c0c95486c44
epicpython <jr3python@gmail.com>
2022-01-20 11:47:11 -0500

    Calculated gamma
    Successfully calculated gamma from test data

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,7 +1,7 @@
 #day 2
 #read from text file
 text_file = open("testdata.txt", "r")
-lines = text_file.readlines()
+lines = text_file.readlines() #global
 #print(lines)
 
 """
@@ -12,28 +12,30 @@ for line in lines:
     betterlines.append(newline)
 """
 #print(betterlines)
-
-#just doing the first digit in each line
-numones = 0
-numzeros = 0
 gamma = ""
-for line in lines:
-    if line[0] == "1": #data file contains strings
-        numones += 1
-    elif line[0] == "0":
-        numzeros += 1
-#determine if more ones or zeros and add correponding number to answer
-#this is to calculate gamma rate
-if numones > numzeros:
-    gamma = gamma + "1"
-elif numones < numzeros:
-    gamma = gamma + "0"
-else:
-    #ones and zeros are equal
-    print("error, equal amounts of 1 and 0")
-    
-print("numones is", numones)
-print("numzeros is", numzeros)
+
+def calc_gamma(gamma,index):
+    numones = 0
+    numzeros = 0
+    for line in lines:
+        if line[index] == "1": #data file contains strings
+            numones += 1
+        elif line[index] == "0":
+            numzeros += 1
+    #determine if more ones or zeros and add correponding number to answer
+    #this is to calculate gamma rate
+    if numones > numzeros:
+        gamma = gamma + "1"
+    elif numones < numzeros:
+        gamma = gamma + "0"
+    else:
+        #ones and zeros are equal
+        print("error, equal amounts of 1 and 0")
+    return gamma
+
+for i in range(5): #5 for test data, change to 12 for real data
+    gamma = calc_gamma(gamma,i)
+
 print("gamma is", gamma)
         
         
