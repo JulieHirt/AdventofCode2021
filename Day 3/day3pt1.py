bf76da2c1cfabc8c3fb0e1aea61412ca2e620046
epicpython <jr3python@gmail.com>
2022-01-20 13:57:38 -0500

    Day 3 part 2 started
    Determine if there are more 1s or 0s in the first digit
    if more ones, keep all numbers with 1 in first place
    if more zeros, keep all numbers with 0 in first place

--- Day 3/day3pt1.py
+++ Day 3/day3pt1.py
@@ -1,9 +1,8 @@
 #day 2
 #read from text file
-text_file = open("testdata.txt", "r")
+text_file = open("day3data.txt", "r")
 lines = text_file.readlines() #global
 #print(lines)
-
 
 """
 #convert all to int and make new list
@@ -13,46 +12,46 @@ for line in lines:
     betterlines.append(newline)
 """
 #print(betterlines)
+gamma = ""
 
-def is_0or1_most(index):
+def calc_gamma(gamma,index):
     numones = 0
     numzeros = 0
-    mostcommon = "error" #set to 0 or 1
     for line in lines:
         if line[index] == "1": #data file contains strings
             numones += 1
         elif line[index] == "0":
             numzeros += 1
-    if numones == 0 and numzeros == 0: #in case it gets to /n at the end
-        print("error no ones or zeros")
-        return mostcommon
-    #determine if more ones or zeros
+    #determine if more ones or zeros and add correponding number to answer
+    #this is to calculate gamma rate
     if numones > numzeros:
-        mostcommon = "1"
+        gamma = gamma + "1"
     elif numones < numzeros:
-        mostcommon = "0"
+        gamma = gamma + "0"
     else:
         #ones and zeros are equal
-        mostcommon = "1"
-        print("equal amounts of 1 and 0")
-    return mostcommon
+        print("error, equal amounts of 1 and 0")
+    return gamma
 
-
-
-index = 0 #doing for first digit only
-digit = is_0or1_most(0)
-print(digit)
-remaininglines = [] #store the binary numbes not yet eliminated from list
-for line in lines:
-    if line[index] == digit:
-        remaininglines.append(line)
-
-print(remaininglines)
-"""
 for i in range(12): #5 for test data, 12 for real data
-    print(i)
+    gamma = calc_gamma(gamma,i)
 
-   
+print("gamma is", gamma)
+        
+
+#epsilon is the opposite of gamma
+#every 0 in gamma is a 1 in epsilon
+epsilon = ""
+for i in range(len(gamma)):
+    if gamma[i] == "0":
+        epsilon = epsilon + "1"
+    elif gamma[i] == "1":
+        epsilon = epsilon + "0"
+    else:
+        print("error, character in gamma is not 0 or 1")
+
+print("epsilon is", epsilon)
+        
 #convert to decimal
 #note: bin() is used to go from binary to decimal
 gamma_int = int(gamma, 2) # 2 us because we are staring in binary (base 2)
@@ -64,6 +63,5 @@ print("epsilon as decimal", epsilon_int)
 answer = gamma_int*epsilon_int
 
 print("gamma*epsilon =",answer)
-"""
 
 input("press enter to exit")
