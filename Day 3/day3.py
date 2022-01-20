62e4288668febff878933936178967a4fb99320c
epicpython <jr3python@gmail.com>
2022-01-20 11:28:42 -0500

    Calculate first bit of gamma
    Looking at just the first number in each line
    determine if there are more 1s or zeros and save corresponding number to gamma

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -14,8 +14,29 @@ for line in lines:
 #print(betterlines)
 
 #just doing the first digit in each line
+numones = 0
+numzeros = 0
+gamma = ""
 for line in lines:
-    print(line[0]) #print the first digit in the line
+    if line[0] == "1": #data file contains strings
+        numones += 1
+    elif line[0] == "0":
+        numzeros += 1
+#determine if more ones or zeros and add correponding number to answer
+#this is to calculate gamma rate
+if numones > numzeros:
+    gamma = gamma + "1"
+elif numones < numzeros:
+    gamma = gamma + "0"
+else:
+    #ones and zeros are equal
+    print("error, equal amounts of 1 and 0")
+    
+print("numones is", numones)
+print("numzeros is", numzeros)
+print("gamma is", gamma)
+        
+        
 
 
 input("press enter to exit")
